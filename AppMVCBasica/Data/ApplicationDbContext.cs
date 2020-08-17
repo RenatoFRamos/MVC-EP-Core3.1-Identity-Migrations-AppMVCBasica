22b3d90bc971d3ebc4386bd67ecbb1a948612d9e
RenatoFRamos <ramos.renato@live.com>
2020-08-17 14:16:50 -0300

    Inicial

---
+++ AppMVCBasica/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using AppMVCBasica.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AppMVCBasica.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Produto> Enderecos { get; set; }
+        public DbSet<Fornecedor> Forcenedores { get; set; }
+        public DbSet<Endereco> Produtos { get; set; }
+    }
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+# MVC-EP-Core3.1-Identity-MigrationsAppMVCBasica
+
+## Treinamento
+Dominando o ASP.NET MVC Core
+
+## Instrutor
+Eduardo Pires
+
+## Site
+[Desenvolvedor.io](https://Desenvolvedor.io)"# MVC-EP-Core3.1-Identity-MigrationsAppMVCBasica" 
